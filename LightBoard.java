f767b0f84aa7012c18741f6fa91389f8a90e8863
michaelz126 <1zhang.michael@gmail.com>
2025-05-04 22:00:23 +0000

    finished

--- LightBoard.java
+++ LightBoard.java
@@ -1,19 +1,41 @@
 public class LightBoard
 {
- /** The lights on the board, where true represents on and false represents off.
- */
- private boolean[][] lights;
- /** Constructs a LightBoard object having numRows rows and numCols columns.
- * Precondition: numRows > 0, numCols > 0
- * Postcondition: each light has a 40% probability of being set to on.
- */
+    
+ public boolean[][] lights;
+
+ public void setLights(boolean[][] arr)
+ {
+  lights = arr;
+ }
  public LightBoard(int numRows, int numCols)
- { /* to be implemented in part (a) */ }
- /** Evaluates a light in row index row and column index col and returns a status
- * as described in part (b).
- * Precondition: row and col are valid indexes in lights.
- */
+ { 
+    lights = new boolean[numRows][numCols];
+    for (int r = 0; r < numRows; r++) {
+        for (int c = 0; c < numCols; c++) {
+            if (Math.random() < 0.4) lights[r][c] = true; 
+        }
+    }
+  }
+
  public boolean evaluateLight(int row, int col)
- { /* to be implemented in part (b) */ }
- // There may be additional instance variables, constructors, and methods not shown.
-} 
+ { 
+  int on = 0;
+  for (int r = 0; r < lights.length; r++)
+  if (lights[r][col]) on++;
+
+  if (lights[row][col] == true && on % 2 == 0) return false;
+  if (lights[row][col] == false && on % 3 == 0) return true;
+  return lights[row][col];
+ }
+
+  public double test() {
+    double on = 0;
+    for (boolean[] row : lights) {
+        for (boolean b : row) {
+            if (b) on++;
+        }
+        
+    }
+    return on / (lights.length * lights[0].length);
+  }
+}
